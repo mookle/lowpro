5f2b1b3a567dccf160df67bfac4defbdbba646fb
mookle <mwaddilove@gmail.com>
2016-07-06 00:22:46 -0700

    Ansible playbooks for OSX and Linux

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,12 +3,14 @@
 readonly PROVISION_DIR="$(HOME)/src/lowpro"
 
 osx_setup() {
+    playbook=osx.yml
     local homebrew='https://raw.githubusercontent.com/Homebrew/install/master/install'
     /usr/bin/ruby -e "$(curl -fsSL $homebrew)"
     brew install git ansible
 }
 
 linux_setup() {
+    playbook=linux.yml
     sudo apt-get update -y
     sudo apt-get install git ansible -y
 }
@@ -26,4 +28,4 @@ git clone https://github.com/mookle/lowpro $PROVISION_DIR
 cd $PROVISION_DIR
 
 # Setup localhost
-ansible-playbook -i hosts --ask-become-pass localhost.yml
+ansible-playbook -i hosts --ask-become-pass $playbook
