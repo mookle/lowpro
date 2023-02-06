a3a5383a0605b5ad43fc7087f48dd508a9b63662
mookle <mwaddilove@gmail.com>
2023-02-06 08:59:33 +0000

    Additional Godot support:
    - Better snippets
    - Explicit indentation
    - Placeholder var for executable

--- roles/neovim/files/config/lua/config/vim-godot.lua
+++ roles/neovim/files/config/lua/config/vim-godot.lua
@@ -3,3 +3,4 @@ require('lspconfig').gdscript.setup {
     capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 }
 
+--vim.g.godot_executable = ""
