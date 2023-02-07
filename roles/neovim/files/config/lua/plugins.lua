05de8f6a679f1418c8a6bb68f199e5e1c99ca188
mookle <mwaddilove@gmail.com>
2023-02-07 22:03:38 +0000

    Move inline diagnostics to a dedicated buffer

--- roles/neovim/files/config/lua/plugins.lua
+++ roles/neovim/files/config/lua/plugins.lua
@@ -25,6 +25,28 @@ return {
         },
         config = true,
     },
+    -- display diagnostics in a list
+    {
+        "folke/trouble.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            vim.diagnostic.config({
+                virtual_text = false,
+                float = { border = "rounded" },
+                use_diagnostic_signs = true,
+            })
+
+            local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+            for type, icon in pairs(signs) do
+                local hl = "DiagnosticSign" .. type
+                vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+            end
+
+            require("utils").map("n", "<space>t", "<cmd>TroubleToggle<cr>")
+        end,
+    },
     -- colorschemes
     { "srcery-colors/srcery-vim" },
     {
