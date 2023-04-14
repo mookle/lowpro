2292f981790db491687c134e339c29ca0774af9a
mookle <mwaddilove@gmail.com>
2023-04-14 12:28:26 +0100

    Replace / with a fuzzysearch plugin

--- roles/neovim/files/config/lua/plugins/pounce_nvim.lua
+++ roles/neovim/files/config/lua/plugins/pounce_nvim.lua
@@ -2,9 +2,8 @@ return {
     "rlane/pounce.nvim",
     config = function()
         local map = require("utils").map
-        map("n", "/", "<cmd>Pounce<cr>")
-        map("n", "?", "<cmd>PounceRepeat<cr>")
-        map("v", "/", "<cmd>Pounce<cr>")
+        map("n", "?", "<cmd>Pounce<cr>")
+        map("v", "?", "<cmd>Pounce<cr>")
     end,
 }
 
