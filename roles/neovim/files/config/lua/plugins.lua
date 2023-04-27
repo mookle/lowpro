40bd66587f2535632c02e601c3f15204b251b422
mookle <mwaddilove@gmail.com>
2023-04-27 19:57:37 +0100

    Improve tmux config:
    - Install plugin manager
    - Tweak visuals
    - Add nvim <-> tmux navigation support
    - Improve prefix combo

--- roles/neovim/files/config/lua/plugins.lua
+++ roles/neovim/files/config/lua/plugins.lua
@@ -58,4 +58,15 @@ return {
     { 'ntpeters/vim-better-whitespace' },
     -- dynamic column limit display
     { 'Bekaboo/deadcolumn.nvim' },
+    -- unified nvim/tmux buffer/pane navigation
+    {
+        'christoomey/vim-tmux-navigator',
+        config = function()
+            local map = require("utils").map
+            map("n", "<c-n>", "<cmd>TmuxNavigateLeft<cr>")
+            map("n", "<c-e>", "<cmd>TmuxNavigateDown<cr>")
+            map("n", "<c-i>", "<cmd>TmuxNavigateUp<cr>")
+            map("n", "<c-o>", "<cmd>TmuxNavigateRight<cr>")
+        end ,
+    },
 }
