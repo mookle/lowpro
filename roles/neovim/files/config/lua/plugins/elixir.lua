337e94cf919a63d35558af1e75b1cb90e6614282
mookle <mwaddilove@gmail.com>
2023-07-13 10:36:42 +0100

    Upgrade to elixir-tools plugin [neovim]

--- roles/neovim/files/config/lua/plugins/elixir.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "mhanberg/elixir.nvim",
-    dependencies = {
-        "nvim-lua/plenary.nvim"
-    },
-    opts = {
-        cmd = "/Users/mathew/.elixir-ls/release/language_server.sh",
-    },
-}
