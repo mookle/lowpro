4e28570aa445e4f602467cb85d103f29a6a460e2
mookle <aaf4c@waddi.love>
2024-02-20 15:02:15 +0000

    Tweak diagnostic list [neovim]:
    - Fix/improve status-column icons
    - Move list to RHS
    - Close when empty
    - No preview when browsing
    - Remove redundant vim.diagnostic config

--- roles/neovim/files/config/lua/plugins/trouble_nvim.lua
+++ roles/neovim/files/config/lua/plugins/trouble_nvim.lua
@@ -3,19 +3,19 @@ return {
     dependencies = {
         "nvim-tree/nvim-web-devicons",
     },
-    config = function()
-        vim.diagnostic.config({
-            virtual_text = false,
-            float = { border = "rounded" },
-            use_diagnostic_signs = true,
-        })
-
-        local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+    config = function(args)
+        local signs = { Error = "", Warn = "", Hint = "", Info = "" }
         for type, icon in pairs(signs) do
             local hl = "DiagnosticSign" .. type
             vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
         end
-
         require("utils").map("n", "<space>d", "<cmd>TroubleToggle<cr>")
+        require("trouble").setup(args.opts)
     end,
+    opts = {
+        auto_close = true,
+        auto_preview = false,
+        position = "right",
+        width = 80,
+    },
 }
